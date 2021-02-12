ce487c50bb169933a10deacaf085a6811318969b
guindosaros <47152090+guindosaros@users.noreply.github.com>
2021-02-12 15:03:02 +0000

    update code

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # usermail = "mohamedsaros@gmail.com"
 # userpass = "Momo2020"
-# python main.py --username mohamedsaros@gmail.com --password Momo2020 --url https://www.nike.com/fr/launch/t/womens-lahar-low-black --shoe-size EU 38 --driver-type chrome
+# python main.py --username mohamedsaros@gmail.com --password Momo2020 --url https://www.nike.com/fr/launch/t/womens-lahar-low-black --secure 333 --shoe-size EU 38 --driver-type chrome
 
 logging.config.dictConfig({
     "version": 1,
@@ -50,7 +50,7 @@ SUBMIT_BUTTON_XPATH = "/html/body/div[2]/div/div/div[2]/div/div/div/div/div[2]/d
 LOGGER = logging.getLogger()
 
 
-def run(driver,username,password,url,shoe_size,login_time=None,release_time=None,page_load_timeout=None,num_retries=None,dont_quit=True):
+def run(driver,username,password,url,secure,shoe_size,login_time=None,release_time=None,page_load_timeout=None,num_retries=None,dont_quit=True):
     
     driver.maximize_window()
     driver.set_page_load_timeout(page_load_timeout)
@@ -93,31 +93,74 @@ def run(driver,username,password,url,shoe_size,login_time=None,release_time=None
             LOGGER.info("Le chargement des pages a été interrompu, mais se poursuit quand même")
             
         try:
-            select_shoe_size(driver=driver, shoe_size=shoe_size)
+           select_size = select_shoe_size(driver=driver, shoe_size=shoe_size)
         except Exception as e:
+            select_size = False
             # Try refreshing page since you can't click Buy button without selecting size (except if size parameter passed in)
-            LOGGER.exception("N'a pas réussi à sélectionner la pointure : " + str(e))
+            LOGGER.exception("Erreur choix de la pointure" + str(e))
             # continue
-        
-        try:
-            click_add_panier_button(driver=driver)
-        except Exception as e:
-            LOGGER.exception("Erreur d'Ajout dans panier: " + str(e))                                
-            six.reraise(Exception, e, sys.exc_info()[2])
-        
-        try: 
-            LOGGER.info("En Attente verification panier")
-            time.sleep(10)
-            # wait_until_visible(driver, xpath="//a[@class='shopping-cart-button']/span[1]", duration=10)
-            panier_url = "https://www.nike.com/fr/cart"
-            LOGGER.info("Page de demande : " + panier_url)
-            driver.get(panier_url)
-        except TimeoutException:
-            LOGGER.info("Votre panier est vide")
-        
             
+        if select_size :
+            try:
+                add_panier_status = click_add_panier_button(driver=driver)
+            except Exception as e:
+                add_panier_status = False
+                LOGGER.exception("Erreur d'Ajout dans panier: " + str(e))                                
+                six.reraise(Exception, e, sys.exc_info()[2])
+                
+            if add_panier_status :
+                try: 
+                    LOGGER.info("initialisation du  panier")
+                    time.sleep(6)
+                    panier_url = "https://www.nike.com/fr/cart"
+                    LOGGER.info("Page de demande : " + panier_url)
+                    driver.get(panier_url)
+                    # apelle fonction de verification du panier 
+                except TimeoutException:
+                    LOGGER.info("Votre panier est vide")
+
+                try: 
+                    LOGGER.info("Verifacation du panier")
+                    verif_panier = check_panier(driver) 
+                except TimeoutException:
+                    verif_panier = False
+                    LOGGER.exception("verif_panier" + str(e))
+                    
+                # Quand panier contient au moins un element a son sein
+                if verif_panier :
+                    try:
+                        try:
+                            check_url = 'https://www.nike.com/fr/fr/checkout'
+                            LOGGER.info("Attendre le Boutton de payement")
+                            wait_until_visible(driver, xpath="//div[@class='ncss-col-sm-12 css-gajhq5']/button[1]", duration=10)
+                            driver.get(check_url)
+                            LOGGER.info("Page de demande : " + check_url)
+                            time.sleep(3)
+                            status_check_adresse = check_livraison_adresse(driver=driver)
+                            carte_paiement_status = check_carte_paiement_and_secure(driver,secure)
+                        except TimeoutException:
+                            LOGGER.info("Le chargement des pages a été interrompu, mais se poursuit quand même")
+                    except Exception as e:
+                        LOGGER.exception("Impossible d'acceder a la page de payement" + str(e))
+                    
+                    if status_check_adresse :
+                        LOGGER.info("Adresse de livraison ok verification carte paiement")
+                        if carte_paiement_status :
+                            LOGGER.info("carte de paiment present peut passer a la confirmation de la commande")
+                            
+                        else:
+                            LOGGER.info("votre compte manque de carte de paiement")
+                    else:
+                        LOGGER.info("votre compte manque d'adresse de livraison")
+                else:
+                    LOGGER.info("votre panier est vide")
+            else:
+                LOGGER.info("Erreur d'Ajout du basket au panier")
+        else:
+           LOGGER.info("n'a pas réussi à sélectionner la pointure car elle est indisponible:")
+
     except Exception as e:
-        print('execption',str(e))
+        print('exeception',str(e))
         if num_retries and num_retries_attempted < num_retries:
             num_retries_attempted += 1
             skip_add_address = False
@@ -134,13 +177,34 @@ def run(driver,username,password,url,shoe_size,login_time=None,release_time=None
         
     # driver.quit()
 
-
+def check_panier(driver):
+    time.sleep(10)
+    status = False
+    
+    xpath = "//p[@class='css-1lavku9 e181ly3q6']/span[1]"
+    valeur = "0,00 €"
+    try:
+        panier = driver.find_element_by_xpath("//p[@class='css-1lavku9 e181ly3q6']/span[1]")
+        print('panier element ===>', panier.text)
+        if panier.text == valeur :
+            LOGGER.info("panier vide")
+            status = False
+        else:
+            status = True
+            LOGGER.info(f"la somme total du panier est : {panier.text}")
+    except Exception as e:
+        LOGGER.exception("Impossible d'acceder a la page de payement" + str(e))
+    
+    return status
+    
 def select_shoe_size(driver, shoe_size):
+    time.sleep(10)
     LOGGER.info("Action pour voir si on peut payer l'element")
     wait_until_visible(driver, xpath="//button[@class='ncss-btn-primary-dark btn-lg']", duration=10)
     size_dispo = driver.find_elements_by_xpath("//li[@class='size va-sm-m d-sm-ib va-sm-t ta-sm-c  ']")
     taille_list = []
     disponible = False
+    status = False
     
     for tailles in size_dispo:
         taille = tailles.text
@@ -157,27 +221,54 @@ def select_shoe_size(driver, shoe_size):
     if disponible:
         driver.find_element_by_xpath("//li[@data-qa='size-available']").find_element_by_xpath(f"//button[text()='EU {shoe_size}']").click()
         LOGGER.info("Pointure chosir avec success")
-    else:
-        LOGGER.info("Basket indisponible pour pointure")
+        status = True
+    
+    return status
 
        
 def click_add_panier_button(driver):
-    xpath = "//button[@class='ncss-btn-primary-dark btn-lg']"
+    time.sleep(7)
+    status = False
+    try:
+        xpath = "//button[@class='ncss-btn-primary-dark btn-lg']"
+        LOGGER.info("En attendant  que le bouton ajouter dans panier  soit présent")
+        element = wait_until_present(driver, xpath=xpath, duration=10) 
+        LOGGER.info("Ajouter au panier")
+        driver.execute_script("arguments[0].click();", element)
+        LOGGER.info("Basket Ajouter au Panier avec Success")
+        status = True
+    except Exception as e:
+        LOGGER.exception("Impossible d'acceder a la page de payement" + str(e))
+    return status
     
-    LOGGER.info("En attendant  que le bouton ajouter dans panier  soit présent")
-    element = wait_until_present(driver, xpath=xpath, duration=10) 
-    
-    LOGGER.info("Ajouter au panier")
-    # driver.find_element_by_xpath(xpath).click()
-    driver.execute_script("arguments[0].click();", element)
-    LOGGER.info("Basket Ajouter au Panier avec Success")
-    
+def check_livraison_adresse(driver):
+    time.sleep(4)
+    status = False
+    try:
+        LOGGER.info("Verification de l'adresse de livraison")
+        xpath = "//i[@class='g72-check text-color-success mr4-sm']"
+        wait_until_present(driver, xpath=xpath, duration=10)
+        LOGGER.info("Adresse de livraison disponible")
+        status = True
+    except Exception as e:
+        LOGGER.exception("Adresse de livraison non enregistre " + str(e))
+        status = False
+    return status
 
-# def go_to_panier(driver):
-#    try:
-#     LOGGER.info("Redirection vers le panier")
-#     wait_until_visible(driver, xpath="//a[@class='shopping-cart-button']/span[1]", duration=10)
-    
+def check_carte_paiement_and_secure(driver,secure):
+    time.sleep(4)
+    status = False
+    try:
+        LOGGER.info("verification de presence de carte de paiement")
+        xpath = "//label[@class='ncss-label pl4-sm pt3-sm pb3-sm pr4-sm pt2-lg pb2-lg u-full-width']"
+        wait_until_present(driver, xpath=xpath, duration=10)
+        LOGGER.info("carte paiement disponible livraison disponible")
+        status = True
+    except Exception as e:
+        LOGGER.exception("Adresse de livraison non enregistre " + str(e))
+        status = False
+    return status
+
 # fonction de connexion au Site 
 def login(driver, username, password):
     try:
@@ -272,6 +363,7 @@ if __name__ == "__main__":
     parser.add_argument("--username", required=True)
     parser.add_argument("--password", required=True)
     parser.add_argument("--url", required=True)
+    parser.add_argument("--secure", required=True)
     parser.add_argument("--shoe-size", required=True)
     parser.add_argument("--driver-type", default="firefox", choices=("firefox", "chrome"))
     parser.add_argument("--headless", action="store_true")
@@ -319,4 +411,4 @@ if __name__ == "__main__":
     else:
         raise Exception("Specified web browser not supported, only Firefox and Chrome are supported at this point")
     
-    run(driver=driver,username=args.username, password=args.password, url=args.url,shoe_size=args.shoe_size,login_time=args.login_time,release_time=args.release_time,page_load_timeout=args.page_load_timeout,num_retries=args.num_retries,dont_quit=args.dont_quit)
+    run(driver=driver,username=args.username, password=args.password, url=args.url, secure=args.secure,shoe_size=args.shoe_size,login_time=args.login_time,release_time=args.release_time,page_load_timeout=args.page_load_timeout,num_retries=args.num_retries,dont_quit=args.dont_quit)
