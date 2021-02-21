8f44b28c4552719e930f46253710db24d53c0ef4
guindosaros <47152090+guindosaros@users.noreply.github.com>
2021-02-21 17:50:56 +0000

    update app

--- app/interface.py
+++ app/interface.py
@@ -1,64 +1,56 @@
 import PySimpleGUI as sg
-"""
-    Demonstration of MENUS!
-    How do menus work?  Like buttons is how.
-    Check out the variable menu_def for a hint on how to 
-    define menus
-"""
-def second_window():
 
-    layout = [[sg.Text('The second form is small \nHere to show that opening a window using a window works')],
-              [sg.OK()]]
 
-    window = sg.Window('Second Form', layout)
-    event, values = window.read()
+
+def app():
+
+    sg.theme('DarkAmber')  # No gray windows please!
+    # sg.set_options(element_padding=(0, 0))
+
+     # ------ Menu Definition ------ #
+    menu_def = [['&Accueil'],
+                ['&Configuration'],
+                ['&Aide'],
+                ['&Quitter'],]
+
+    col1 = sg.Column([
+    # Categories frame
+    [ sg.Frame('SNKRS : ', [[ sg.Text('Acheter Automatiquement vos Basket sur SNKRS '),]],)],
+    # Information frame
+    [ sg.Frame('Information Basket:', [[ sg.Text(),  sg.Column([
+                                    [sg.Text('Basket-url', size=(10, 1)), sg.InputText(key='-NAME-')],
+                                    [sg.Text('Taille-Basket', size=(10, 1)), sg.InputText(key='-ADDRESS-')],
+                                    [sg.Text('Waitime', size=(10, 1)), sg.InputText(key='-PHONE-')],
+                                    ], size=(235, 350), pad=(50, 50))]])], ], pad=(50, 50))
+                                    
+    col3 =  sg.Column([[ sg.Frame('Actions:', [[ sg.Column([[ sg.Button('Save'),  sg.Button(
+    'Clear'),  sg.Button('Delete'), ]], size=(450, 60), pad=(0, 0))]])]], pad=(0, 0))
+    
+    # STEP 1 define the layout
+    layout = [ 
+                [sg.Menu(menu_def, tearoff=False, pad=(20,1))],
+                [col1],
+                [col3]
+            ]
+
+    #STEP 2 - create the window
+    window = sg.Window('SNKRS Boot Application', 
+    layout, grab_anywhere=True,
+    icon= 'skrs.ico',
+    location = (50,50)
+    )
+
+    # STEP3 - the event loop
+    while True:
+        event, values = window.read()   # Read the event that happened and the values dictionary
+        print(event, values)
+        if event == sg.WIN_CLOSED or event == 'Exit':     # If user closed window with X or if user clicked "Exit" button then exit
+            break
+        if event == 'Button':
+            print('You pressed the button')
     window.close()
 
-def test_menus():
 
 
-    sg.theme('LightGreen')
-    sg.set_options(element_padding=(0, 0))
-
-    # ------ Menu Definition ------ #
-    menu_def = [['&Accueil'],['&Configuration'],['&Aide'],['&Aide'],
-                ]
-
-    right_click_menu = ['Unused', ['Right', '!&Click', '&Menu', 'E&xit', 'Properties']]
-
-
-    # ------ GUI Defintion ------ #
-    layout = [
-              [sg.Menu(menu_def, tearoff=False, pad=(20,1))],
-              [sg.Text('Click right on me to see right click menu')],
-              [sg.Output(size=(60,20))],
-              [sg.ButtonMenu('ButtonMenu',key='-BMENU-', menu_def=menu_def[0])],
-              ]
-
-    window = sg.Window("Windows-like program",
-                       layout,
-                       default_element_size=(12, 1),
-                       grab_anywhere=True,
-                       right_click_menu=right_click_menu,
-                       default_button_element_size=(12, 1))
-
-    # ------ Loop & Process button menu choices ------ #
-    while True:
-        event, values = window.read()
-        if event is None or event == 'Exit':
-            return
-        print('Event = ', event)
-        # ------ Process menu choices ------ #
-        if event == 'About...':
-            window.disappear()
-            sg.popup('About this program','Version 1.0', 'PySimpleGUI rocks...', grab_anywhere=True)
-            window.reappear()
-        elif event == 'Open':
-            filename = sg.popup_get_file('file to open', no_window=True)
-            print(filename)
-        elif event == 'Properties':
-            second_window()
-        elif event == '-BMENU-':
-            print('You selected from the button menu:', values['-BMENU-'])
-
-test_menus()
+if __name__ == '__main__':      
+        app()
---
+++ test.py
@@ -0,0 +1,86 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@guindosaros 
+PySimpleGUI
+/
+PySimpleGUI
+173
+5.6k929
+Code
+Issues
+658
+Pull requests
+Actions
+Projects
+1
+Wiki
+Security
+Insights
+PySimpleGUI/DemoPrograms/Demo_Column_And_Frames.py /
+@PySimpleGUI
+PySimpleGUI WIN_CLOSED bulk update. No more test for event is None, use WIN_CLOSE…
+…
+Latest commit 0076b46 on 7 May 2020
+ History
+ 1 contributor
+53 lines (43 sloc)  2.77 KB
+  
+# this one long import has the effect of making the code more compact as there is no 'sg.' prefix required for Elements
+import PySimpleGUI as sg
+from PySimpleGUI import InputCombo, Combo, Multiline, ML, MLine, Checkbox, CB, Check, Button, B, Btn, ButtonMenu, Canvas, Column, Col, Combo, Frame, Graph, Image, InputText, Input, In, Listbox, LBox, Menu, Multiline, ML, MLine, OptionMenu, Output, Pane, ProgressBar, Radio, Slider, Spin, StatusBar, Tab, TabGroup, Table, Text, Txt, T, Tree, TreeData,  VerticalSeparator, Window, Sizer
+
+"""
+    Demo Columns and Frames
+    Demonstrates using mixture of Column and Frame elements to create a nice window layout.
+    A couple of the concepts shown here include:
+    * Using Columns and Frames with specific sizes on them
+    * Importing all required classes so that "sg." is not required on any objects. This makes the code more compact and readable
+    
+    There are 3 columns.  Two are side by side at the top and the third is along the bottom
+"""
+
+sg.theme('GreenTan')
+
+col2 = Column([[Frame('Accounts:', [[Column([[Listbox(['Account '+str(i) for i in range(1, 16)],
+                                                      key='-ACCT-LIST-', size=(15, 20)), ]], size=(150, 400))]])]], pad=(0, 0))
+
+col1 = Column([
+    # Categories frame
+    [Frame('Categories:', [[ Radio('Websites', 'radio1', default=True, key='-WEBSITES-', size=(10, 1)),
+                            Radio('Software', 'radio1', key='-SOFTWARE-',  size=(10, 1))]],)],
+    # Information frame
+    [Frame('Information:', [[Text(), Column([[Text('Account:')],
+                                     [Input(key='-ACCOUNT-IN-', size=(19, 1))],
+                                     [Text('User Id:')],
+                                     [Input(key='-USERID-IN-', size=(19, 1)),
+                                      Button('Copy', key='-USERID-')],
+                                     [Text('Password:')],
+                                     [Input(key='-PW-IN-', size=(19, 1)),
+                                      Button('Copy', key='-PASS-')],
+                                     [Text('Location:')],
+                                     [Input(key='-LOC-IN-', size=(19, 1)),
+                                      Button('Copy', key='-LOC')],
+                                     [Text('Notes:')],
+                                     [Multiline(key='-NOTES-', size=(25, 5))],
+                                     ], size=(235, 350), pad=(0, 0))]])], ], pad=(0, 0))
+
+col3 = Column([[Frame('Actions:', [[Column([[Button('Save'), Button(
+    'Clear'), Button('Delete'), ]], size=(450, 45), pad=(0, 0))]])]], pad=(0, 0))
+
+layout = [[col1, col2], [col3]]
+
+window = Window('Passwords', layout)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
